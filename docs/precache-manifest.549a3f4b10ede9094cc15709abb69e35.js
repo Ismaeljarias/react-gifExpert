2ad899513c51fef771ccba8ad429ba6d6759cc1d
Ismael Arias <ismaeljarias@gmail.com>
2020-07-02 09:35:05 -0400

    docs added

---
+++ docs/precache-manifest.549a3f4b10ede9094cc15709abb69e35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5076f21c36c341d824969be16f6a2678",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34419ad55e995f2da6ee",
+    "url": "/static/css/main.72cd0019.chunk.css"
+  },
+  {
+    "revision": "89410fccec1793ce5168",
+    "url": "/static/js/2.30d82008.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.30d82008.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34419ad55e995f2da6ee",
+    "url": "/static/js/main.a9c05b67.chunk.js"
+  },
+  {
+    "revision": "6a26a6a45f9e65ecd26f",
+    "url": "/static/js/runtime-main.0ba3af82.js"
+  }
+]);
